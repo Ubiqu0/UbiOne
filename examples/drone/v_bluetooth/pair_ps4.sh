b6a94431e0102f892c85894bb5f3cca8d4a6cec6
Miguel Won <miguelwon@gmail.com>
2022-10-08 13:44:08 +0100

    Deleted the file from the git repository

--- examples/drone/v_bluetooth/pair_ps4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mac="70:20:84:50:43:A8"
-
-if bluetoothctl info "$mac" | grep -q 'Connected: yes'; then
-    echo "already connected  $mac"
-else
-    echo -e "connect ${mac} \n" | bluetoothctl
-fi
